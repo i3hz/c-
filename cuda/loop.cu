f644d40a49aa2f6630a71b9409e9d4bbad5d7f2b
i3hz <vedthorat1029@gmail.com>
2025-10-15 10:51:12 +0000

    loops on a single thread

---
+++ cuda/loop.cu
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+__global__ void loop(int N)
+{
+  
+    printf("This is iteration number %d\n", threadIdx.x);
+
+}
+
+int main()
+{
+
+
+  int N = 10;
+  loop<<<1,N>>>(N);
+  cudaDeviceSynchronize();
+}
